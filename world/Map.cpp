980efa00b4ce51267d1f33e96975c6847c8070cb
Tik-tak753 <80722076+Tik-tak753@users.noreply.github.com>
2025-12-18 19:50:17 +0200

    Implement world map and level loader

--- world/Map.cpp
+++ world/Map.cpp
@@ -2,13 +2,8 @@
 
 Map::Map(int width, int height)
     : m_size(width, height),
-      m_tiles(width * height, TileFactory::empty())
+      m_tiles(height, QVector<Tile>(width, TileFactory::empty()))
 {
-}
-
-int Map::index(const QPoint& cell) const
-{
-    return cell.y() * m_size.width() + cell.x();
 }
 
 bool Map::isInside(const QPoint& cell) const
@@ -21,14 +16,14 @@ Tile Map::tile(const QPoint& cell) const
 {
     if (!isInside(cell))
         return TileFactory::steel();
-    return m_tiles[index(cell)];
+    return m_tiles[cell.y()][cell.x()];
 }
 
 void Map::setTile(const QPoint& cell, const Tile& tile)
 {
     if (!isInside(cell))
         return;
-    m_tiles[index(cell)] = tile;
+    m_tiles[cell.y()][cell.x()] = tile;
 }
 
 bool Map::isWalkable(const QPoint& cell) const
